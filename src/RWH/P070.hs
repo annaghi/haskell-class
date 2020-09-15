ad1eff5dcecba031748aa81de5ce95bec54202f5
Anna Bansaghi <anna.bansaghi@gmail.com>
2020-09-15 08:24:50 +0200

    Solve exercise 10 on page 70 along with unit and property tests

--- src/RWH/P070.hs
+++ src/RWH/P070.hs
@@ -4,10 +4,15 @@ module RWH.P070
     , Tree(..)
     , height
     , count
+    , Point(..)
+    , Direction(..)
+    , direction
+    , oppositeDirection
     ) where
 
 import Data.List (sortBy)
 import Data.Function (on)
+import Prelude hiding (Left, Right)
 
 
 sortByLength :: [[a]] -> [[a]]
@@ -37,3 +42,41 @@ height (Node _ left right) = 1 + max (height left) (height right)
 count :: Num b => Tree a -> b
 count Empty               = 0
 count (Node _ left right) = 1 + count left + count right
+
+
+data Point =
+    Point Float Float
+    deriving (Show)
+
+
+data Direction
+    = Left
+    | Right
+    | Straight
+    deriving (Show, Eq)
+
+
+direction :: Point -> Point -> Point -> Direction
+direction (Point a1 a2) (Point b1 b2) (Point c1 c2) =
+    if determinant > 0 then
+        Left
+    else if determinant < 0 then
+        Right
+    else
+        Straight
+    where
+        determinant :: Float
+        determinant = ((b1 - a1) * (c2 - a2)) - ((b2 - a2) * (c1 - a1))
+
+
+oppositeDirection :: Direction -> Direction
+oppositeDirection direction =
+    case direction of
+        Left ->
+            Right
+
+        Right ->
+            Left
+
+        Straight ->
+            Straight
--- test/RWH/P070Test.hs
+++ test/RWH/P070Test.hs
@@ -6,11 +6,11 @@ import           Test.Tasty.Hedgehog
 import           Hedgehog
 import qualified Hedgehog.Gen as Gen
 import qualified Hedgehog.Range as Range
-
 import           Utils ((|>))
+
 import qualified Data.List as List
 
-import RWH.P070
+import qualified RWH.P070 as XX
 
 
 tests :: TestTree
@@ -18,51 +18,74 @@ tests =
     testGroup "Page 070"
         [ testGroup "Exercise 06: sortByLength"
             [ testCase "when the list is empty" $
-                (null $ RWH.P070.sortByLength [])
+                (null $ XX.sortByLength [])
                     |> assertEqual "" True
             , testCase "when the list has one item" $
-                RWH.P070.sortByLength [[1,2,3]]
+                XX.sortByLength [[1,2,3]]
                     |> assertEqual "" [[1,2,3]]
             , testCase "when the list has more items" $
-                RWH.P070.sortByLength [[1,2,3],[],[8],[2]]
+                XX.sortByLength [[1,2,3],[],[8],[2]]
                     |> assertEqual "" [[],[8],[2],[1,2,3]]
             , testProperty "mapped to length should result as the same as sorting lengths" $
                 property $ do
                     xs <- forAll $ Gen.list (Range.linear 0 10) genString
-                    (map length . RWH.P070.sortByLength) xs === (List.sort . map length) xs
+                    (map length . XX.sortByLength) xs === (List.sort . map length) xs
             ]
         , testGroup "Exercise 07-08: intersperse"
             [ testCase "when the list is empty" $
-                (null $ RWH.P070.intersperse ',' [])
+                (null $ XX.intersperse ',' [])
                     |> assertEqual "" True
             , testCase "when the list has one item" $
-                RWH.P070.intersperse ',' ["one"]
+                XX.intersperse ',' ["one"]
                     |> assertEqual "" "one"
             , testCase "when the list has more items" $
-                RWH.P070.intersperse ',' ["one","two"]
+                XX.intersperse ',' ["one","two"]
                     |> assertEqual "" "one,two"
             , testProperty "length should result as the same as the sum of the length of concatenation and the number of strings - 1" $
                 property $ do
                     xs <- forAll $ Gen.list (Range.linear 1 10) genString
-                    length (RWH.P070.intersperse ',' xs) === length (concat xs) + length xs - 1
+                    length (XX.intersperse ',' xs) === length (concat xs) + length xs - 1
             ]
         , testGroup "Exercise 09: height"
             [ testCase "when the tree is empty" $
-                RWH.P070.height RWH.P070.Empty
+                XX.height XX.Empty
                     |> assertEqual "" (-1)
             , testCase "when the tree has one node" $
-                RWH.P070.height (RWH.P070.Node 3 RWH.P070.Empty RWH.P070.Empty)
+                XX.height (XX.Node 3 XX.Empty XX.Empty)
                     |> assertEqual "" 0
             , testCase "when the tree has two nodes" $
-                RWH.P070.height (RWH.P070.Node 'a' (RWH.P070.Node 'b' RWH.P070.Empty RWH.P070.Empty) RWH.P070.Empty)
+                XX.height (XX.Node 'a' (XX.Node 'b' XX.Empty XX.Empty) XX.Empty)
                     |> assertEqual "" 1
             , testProperty "should result between (floor (log2 n)) and (n-1)" $
                 property $ do
                     tree <- forAll genTree
                     let
-                        count = RWH.P070.count tree
-                    Hedgehog.assert $ RWH.P070.height tree <= count - 1
-                    Hedgehog.assert $ RWH.P070.height tree >= floor (logBase 2 count)
+                        count = XX.count tree
+                    Hedgehog.assert $ XX.height tree <= count - 1
+                    Hedgehog.assert $ XX.height tree >= floor (logBase 2 count)
+            ]
+        , testGroup "Exercise 10-11: direction"
+            [ testCase "when turning left" $
+                XX.direction (XX.Point 8 2) (XX.Point 4 5) (XX.Point 1 1)
+                    |> assertEqual "" XX.Left
+            , testCase "when turning right" $
+                XX.direction (XX.Point 1 1) (XX.Point 4 5) (XX.Point 8 2)
+                    |> assertEqual "" XX.Right
+            , testCase "when going straight forward" $
+                XX.direction (XX.Point 8 2) (XX.Point 4 5) (XX.Point 0 8)
+                    |> assertEqual "" XX.Straight
+            , testCase "when going straight backward" $
+                XX.direction (XX.Point 8 2) (XX.Point 4 5) (XX.Point 8 2)
+                    |> assertEqual "" XX.Straight
+            , testCase "when going nowhere" $
+                XX.direction (XX.Point 0 0) (XX.Point 0 0) (XX.Point 0 0)
+                    |> assertEqual "" XX.Straight
+            , testProperty "should result as the opposite direction of reversed order" $
+                property $ do
+                    a <- forAll genPoint
+                    b <- forAll genPoint
+                    c <- forAll genPoint
+                    XX.direction a b c === (XX.oppositeDirection $ XX.direction c b a)
             ]
         ]
 
@@ -75,11 +98,18 @@ genString :: MonadGen m => m String
 genString = Gen.list (Range.linear 0 4) Gen.alpha
 
 
-genTree :: MonadGen m => m (RWH.P070.Tree String)
+genTree :: MonadGen m => m (XX.Tree String)
 genTree =
     Gen.recursive Gen.choice
-        [ pure $ RWH.P070.Node "v" RWH.P070.Empty RWH.P070.Empty ]
-        [ RWH.P070.Node "v" RWH.P070.Empty <$> genTree
-        , Gen.subtermM genTree (\tree -> pure $ RWH.P070.Node "v" tree RWH.P070.Empty)
-        , RWH.P070.Node "v" <$> genTree <*> genTree
+        [ pure $ XX.Node "v" XX.Empty XX.Empty ]
+        [ XX.Node "v" XX.Empty <$> genTree
+        , Gen.subtermM genTree (\tree -> pure $ XX.Node "v" tree XX.Empty)
+        , XX.Node "v" <$> genTree <*> genTree
         ]
+
+
+genPoint :: MonadGen m => m XX.Point
+genPoint =
+    XX.Point
+        <$> Gen.float (Range.exponentialFloat (-10) 10)
+        <*> Gen.float (Range.exponentialFloat (-10) 10)
