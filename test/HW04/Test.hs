83f1046368b6c873ef758a992d20c5df41195d91
Anna Bansaghi <anna.bansaghi@gmail.com>
2020-09-20 19:21:00 +0200

    Reorganize tests

--- test/HW04/Test.hs
+++ test/HW04/Test.hs
@@ -3,16 +3,12 @@ module HW04.Test (suite) where
 import Test.Tasty
 
 import HW04.E1Test
-import HW04.E2Test
 import HW04.E3Test
-import HW04.E4Test
 
 
 suite :: TestTree
 suite =
     testGroup "Homework 4"
         [ HW04.E1Test.tests
-        , HW04.E2Test.tests
         , HW04.E3Test.tests
-        , HW04.E4Test.tests
         ]
