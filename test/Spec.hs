4e3c0f21c4a0ce803d31e9bcbd832621990c0c49
Anna Bansaghi <anna.bansaghi@gmail.com>
2020-09-20 19:13:14 +0200

    Reorganizce tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,13 +1,13 @@
 import Test.Tasty
 
 import RWH.P070Test
-import HW04.E1Test
+import HW04.Test
 
 
 main :: IO ()
 main = defaultMain $ testGroup "Tests" $
     [ testGroup "RWH"
         [ RWH.P070Test.tests ]
-    , testGroup "HW04"
-        [ HW04.E1Test.tests ]
+    , testGroup "CIS-194"
+        [ HW04.Test.suite ]
     ]
