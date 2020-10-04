2bcadba6a1ae7d9659430315f566e2037ad44f40
Anna Bansaghi <anna.bansaghi@gmail.com>
2020-10-04 18:50:51 +0200

    Solve Exercise 3 in Homework 4: define foldl with foldr

--- src/HW04/E3.hs
+++ src/HW04/E3.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module HW04.E3
     ( xor
     , map'
@@ -15,6 +17,38 @@ map' f =
     foldr ((:) . f) []
 
 
-myFoldl :: (a -> b -> a) -> a -> [b] -> a
+-- https://www.cs.nott.ac.uk/~pszgmh/fold.pdf
+
+-- myFoldl :: forall a b. (a -> b -> a) -> a -> [b] -> a
+-- myFoldl f base xs =
+--     myFoldl' f xs base
+--     where
+--         myFoldl' :: (a -> b -> a) -> [b] -> a -> a
+--         myFoldl' f [] base = base
+--         myFoldl' f (x:xs) base = myFoldl' f xs (f base x)
+
+-- myFoldl' f [] = v 
+-- myFoldl' f (x:xs) = g x (myFoldl' f xs)    ⟺   myFoldl' = foldr g v
+
+-- myFoldl' f [] = \base -> base
+-- myFoldl' f (x:xs) = g x (myFoldl' f xs)
+-- ⟺ myFoldl' f (x:xs) base = g x (myFoldl' f xs) base
+-- ⟺ myFoldl' f xs (f base x) = g x (myFoldl' f xs) base (h := myFoldl' f xs)
+-- ⟺ h (f base x) = g x h base
+-- ⟹ g = \x h -> (\base -> h (f base x))
+-- ⟹ myFoldl' = foldr (\x h -> (\base -> h (f base x))) (\base -> base)
+
+
+myFoldl :: forall a b. (a -> b -> a) -> a -> [b] -> a
 myFoldl f base xs =
-    foldr (flip f) base $ reverse xs
+    foldr (\x h -> (\base' -> h (f base' x))) (\base' -> base') xs base
+
+--
+-- Highlight the fact that foldr reduces to function of type a -> a:
+--
+-- myFoldl :: forall a. (a -> b -> a) -> a -> [b] -> a
+-- myFoldl f base xs = go base
+--     where
+--         go :: a -> a
+--         go = foldr (\x h -> (\base' -> h (f base' x))) (\base' -> base') xs
+--
