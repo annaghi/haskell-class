b523ccecdcf036b842ecebc741fe38a4b41aa5cc
Anna Bansaghi <anna.bansaghi@gmail.com>
2020-10-08 17:33:23 +0200

    Clarify transformation of primitive recursion to foldr

--- src/HW04/E3.hs
+++ src/HW04/E3.hs
@@ -4,6 +4,9 @@ module HW04.E3
     ( xor
     , map'
     , myFoldl
+    , myFoldl1
+    , myFoldl2
+    , myFoldl3
     ) where
 
 
@@ -17,46 +20,50 @@ map' f =
     foldr ((:) . f) []
 
 
-{-
-https://www.cs.nott.ac.uk/~pszgmh/fold.pdf
 
-myFoldl :: forall a b. (a -> b -> a) -> a -> [b] -> a
-myFoldl f base xs =
-    myFoldl' f xs base
+-- https://www.cs.nott.ac.uk/~pszgmh/fold.pdf
+
+myFoldl1 :: forall a b. (a -> b -> a) -> a -> [b] -> a
+myFoldl1 step base xs =
+    myFoldl' step xs base
     where
         myFoldl' :: (a -> b -> a) -> [b] -> a -> a
-        myFoldl' f [] base = base
-        myFoldl' f (x:xs) base = myFoldl' f xs (f base x)
+        myFoldl' _ [] base' = base'
+        myFoldl' f (y:ys) base' = myFoldl' f ys (f base' y)
 
+
+{-
 myFoldl' f [] = v 
-myFoldl' f (x:xs) = g x (myFoldl' f xs)    ⟺   myFoldl' = foldr g v
+myFoldl' f (y:ys) = g y (myFoldl' f ys)    ⟺   myFoldl' = foldr g v
 
-myFoldl' f [] = \base -> base
-myFoldl' f (x:xs) = g x (myFoldl' f xs)
-⟺ myFoldl' f (x:xs) base = g x (myFoldl' f xs) base
-⟺ myFoldl' f xs (f base x) = g x (myFoldl' f xs) base (h := myFoldl' f xs)
-⟺ h (f base x) = g x h base
-⟹ g = \x h -> (\base -> h (f base x))
-⟹ myFoldl' = foldr (\x h -> (\base -> h (f base x))) (\base -> base)
+myFoldl' f [] = v
+⟹ v = \base' -> base'
+
+myFoldl' f (y:ys) = g y (myFoldl' f ys)
+⟺ myFoldl' f (y:ys) base' = g y (myFoldl' f ys) base'
+⟺ myFoldl' f ys (f base' y) = g y (myFoldl' f ys) base' (h := myFoldl' f ys)
+⟺ h (f base' y) = g y h base'
+⟹ g = \y h base' -> h (f base' y)
+⟹ myFoldl' = foldr (\y h base' -> h (f base' y)) (\base' -> base')
 -}
+myFoldl2 :: (a -> b -> a) -> a -> [b] -> a
+myFoldl2 f base xs =
+    foldr (\y h base' -> h (f base' y)) id xs base
+
+
+--Highlight the fact that foldr reduces to function of type a -> a:
+myFoldl3 :: forall a b. (a -> b -> a) -> a -> [b] -> a
+myFoldl3 f base xs =
+    go base
+    where
+        go :: a -> a
+        go = foldr (\x h base' -> h (f base' x)) (\base' -> base') xs
 
 
 myFoldl :: forall a b. (a -> b -> a) -> a -> [b] -> a
 myFoldl f base xs =
-    -- foldr (\x h -> (\base' -> h (f base' x))) id xs base
     foldr step id xs base
     where
         step :: b -> (a -> a) -> a -> a
         step x h base' =
             h (f base' x)
-
-
-{- 
-Highlight the fact that foldr reduces to function of type a -> a:
-
-myFoldl :: forall a. (a -> b -> a) -> a -> [b] -> a
-myFoldl f base xs = go base
-    where
-        go :: a -> a
-        go = foldr (\x h -> (\base' -> h (f base' x))) (\base' -> base') xs
--}
