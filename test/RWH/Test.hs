480ba11051d1786bb22b6f65193f7a287d0694ea
Anna Bansaghi <anna.bansaghi@gmail.com>
2020-10-13 19:16:01 +0200

    Reorganize RWH tests

---
+++ test/RWH/Test.hs
@@ -0,0 +1,18 @@
+module RWH.Test (suite) where
+
+import Test.Tasty
+
+import RWH.P070Test
+import RWH.P084Test
+import RWH.P097Test
+import RWH.P098Test
+
+
+suite :: TestTree
+suite =
+    testGroup "RWH"
+        [ RWH.P070Test.tests
+        , RWH.P084Test.tests
+        , RWH.P097Test.tests
+        , RWH.P098Test.tests
+        ]
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,21 +1,13 @@
 import Test.Tasty
 
-import RWH.P070Test
-import RWH.P084Test
-import RWH.P097Test
-import RWH.P098Test
+import RWH.Test
 import HW03.Test
 import HW04.Test
 
 
 main :: IO ()
 main = defaultMain $ testGroup "Tests" $
-    [ testGroup "RWH"
-        [ RWH.P070Test.tests
-        , RWH.P084Test.tests
-        , RWH.P097Test.tests
-        , RWH.P098Test.tests
-        ]
+    [ RWH.Test.suite
     , testGroup "CIS-194"
         [ HW03.Test.suite
         , HW04.Test.suite
